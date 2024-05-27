25a500346298a594631bd0a803ea9b906861dab2
Crowdbotics <team@crowdbotics.com>
2024-05-27 08:08:44 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tiny-cell-48117",
+  url: "https://tiny-cell-48117.botics.co",
+  api: "https://tiny-cell-48117.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
